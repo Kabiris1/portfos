c39c2f1d0ed850a451169b063ef77a96bc7099ea
kabir <kabir.almuslim@gmail.com>
2022-11-10 22:56:40 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "/",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "/",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "/",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Thanks for your expertise to my project, Really happy working with you as you did all the work on time which is definitely a great job. I say would he's worth.",
+    imgFile: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "",
+  },
+  {
+    quote:
+      "I am so glad that finally my business went online and it all possible because of Abdul karam thanks bro. You're maintaining my website also happy to have a developer.",
+    imgFile: "/public/client.jpg h-1/2 w-1/2",
+    name: "Syed Raziq ali",
+    company: "Goodluck visa",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "Express.js",
+  "Bootstrap",
+];
